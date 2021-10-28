6840dcf3e003f18af0950e12df57612609055eec
Rizkytama David Mulia Graha <91865327+rizkytama28@users.noreply.github.com>
2021-10-28 22:19:04 +0700

    Add files via upload

---
+++ TugasLibraryUnitTest/TugasLibraryUnitTest/Day.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using TugasLibraryUnitTest;
+
+namespace TugasLibraryUnitTest
+{
+    [TestFixture]
+    public class Day
+    {
+        [Test]
+        public void NameOfDay(string day)
+        {
+            int NameOfDay = (int)System.DateTime.Now.DayOfWeek;
+            
+        }
+    }
+}
